922a25e9918f7ed614199c12c5133b14746065ca
tom <tom.forbes1@gmail.com>
2021-03-14 14:27:20 +0000

    seek and tracking

--- src/server/vlc.ts
+++ src/server/vlc.ts
@@ -1,28 +1,36 @@
 const { exec } = require('child_process');
 
-const logResult = (err, stdout, stderr) => {
-    if (err) {
-        console.log(err)
-    }
-    if (stderr) {
-        console.log(stderr)
-    }
-    console.log(stdout)
-};
-
-const startVlc = (path: string) => exec(`vlc --fullscreen ${path}`, logResult);
-
-const pauseVlc = () => exec(
-    'dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.PlayPause',
-    logResult
+const execute = (cmd: string): Promise<any> => new Promise((resolve, reject) =>
+    exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err)
+            return reject(err);
+        }
+        if (stderr) {
+            console.log(stderr)
+            return reject(err);
+        }
+        console.log(stdout)
+        return resolve(stdout)
+    })
 );
 
-const resumeCmd = 'dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.Play';
-const resumeVlc = () => exec(
-    'dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.Play',
-    logResult
+const start = (path: string) => execute(`vlc --fullscreen ${path}`);
+
+const pause = () => execute('dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.PlayPause');
+
+const resume = () => execute(
+    'dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.Play'
 );
 
-const stopVlc = () => exec('killall vlc', logResult);
+const stop = () => execute('killall vlc');
 
-export { startVlc, pauseVlc, resumeVlc, stopVlc }
+const seek = (us: number) => execute(
+    `dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2   org.mpris.MediaPlayer2.Player.Seek int64:"${us}"`
+)
+
+const position = () => execute(
+    'qdbus org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player Position'
+)
+
+export { start, pause, resume, stop, seek, position }
