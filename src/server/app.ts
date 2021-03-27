c18ca8ebf2659b89b6c34dfee0e92c8a9ba9b105
tom <fake@fake.com>
2021-03-27 15:16:18 +0000

    history

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { getVideosList } = require('./videosList')
+const { getVideoHistory, updateVideoHistory } = require('./videoHistory');
 const Vlc = require('./vlc')
 
 const port = 3000;
@@ -13,9 +14,11 @@ app.options('*', cors());
 app.use(express.static('dist/client'))
 
 app.get('/videos', (req, res) => {
-  getVideosList(videoPath).then(r => {
-    res.send(r)
-  })
+  getVideoHistory()
+    .then(history => getVideosList(videoPath, history))
+    .then(r => {
+      res.send(r)
+    })
 })
 
 app.put(/^\/videos\/(.+)/, (req, res) => {
@@ -46,6 +49,15 @@ app.get('/video/position', (req, res) => {
 
 app.get('/video/length', (req, res) => {
   Vlc.length().then(metadata => res.send(metadata))
+})
+
+app.post(/^\/videos\/viewed\/(.+)/, (req, res) => {
+  const path = req.params[0];
+  updateVideoHistory(`${videoPath}/${path}`, true).then(() => res.send('ok'));
+})
+app.delete(/^\/videos\/viewed\/(.+)/, (req, res) => {
+  const path = req.params[0];
+  updateVideoHistory(`${videoPath}/${path}`, false).then(() => res.send('ok'));
 })
 
 app.listen(port, () => {
---
+++ src/server/videoHistory.ts
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+const FILE_PATH = './video-history.txt';
+
+const getVideoHistory = (): Promise<string[]> => {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(FILE_PATH)) {
+            fs.readFile(FILE_PATH, function(err, data) {
+                if (err) {
+                    return reject(err);
+                }
+                const lines = data.toString().split("\n").map(l => l.trim()).filter(l => l !== '');
+                console.log('history', lines)
+                return resolve(lines);
+            });
+        } else {
+            return resolve([]);
+        }
+    })
+};
+
+const updateVideoHistory = (path: string, viewed: boolean) => {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(FILE_PATH)) {
+            fs.readFile(FILE_PATH, function(err, data) {
+                if (err) {
+                    return reject(err);
+                }
+                const lines = data.toString().split("\n").map(l => l.trim()).filter(l => l !== '');
+                const idx = lines.findIndex(l => l.trim() === path);
+                if (idx >= 0) {
+                    if (viewed) {
+                        lines.splice(idx, 1, path);
+                    } else {
+                        lines.splice(idx, 1);
+                    }
+                } else if (viewed) {
+                    lines.push(path);
+                }
+                fs.writeFile(FILE_PATH, lines.join('\n'), function(err) {
+                    if (err) {
+                        reject(err);
+                    }
+                })
+                resolve(path)
+            })
+        } else if (viewed) {
+            fs.writeFile(FILE_PATH, path, function(err) {
+                if (err) {
+                    reject(err);
+                }
+                resolve(path);
+            })
+        } else {
+            resolve(path);
+        }
+    })
+}
+
+export { getVideoHistory, updateVideoHistory }
